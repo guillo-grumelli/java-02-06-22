5d58bb81ee8ca87c9d9e905f71ed4be8e52b0d7f
guillo-grumelli <85719375+guillo-grumelli@users.noreply.github.com>
2022-06-02 09:20:02 -0300

    Add files via upload

---
+++ ejercicio-individual-main/ejercicio individual/main.js
@@ -0,0 +1,45 @@
+// let artistas=["Olivia Rodrigo", "Cavetown","Vic Fuentes"]
+// console.log(artistas[2])
+// //la posición del array comienza desde el 0
+// //console.log --> muestra en consola
+
+// let canciones=["happier", "Meteor Shower", "Bedless"]
+// console.log(canciones, "canciones en la playlist")
+// //muestra el array completo y le suma texto
+
+// console.log(canciones.length)
+// //muestra la cantidad de elementos en el array
+
+// let comidas=["asado", "pizza", "hamburguesas"]
+// let países=["argentina","italia","estados unidos"]
+// let paisesycomidas= [...comidas, ...países ]
+// console.log(paisesycomidas)
+
+// //une los arrays en una sola lista
+
+//  let comidas=["asado", "pizza", "hamburguesas"]
+//  let países=["argentina","italia","estados unidos"]
+//  let menu=[comidas, países]
+//  console.log(menu)
+//  console.log(menu[0][0])
+//selecciona un elemento de un array
+
+// let comidas=["asado", "pizza", "hamburguesas"]
+// comidas.push("sushi")
+// console.log(comidas)
+//agrega un elemento al array
+
+// let comidas=["asado", "pizza", "hamburguesas","sushi"]
+// comidas.pop()
+// console.log(comidas)
+//elimina el último elemento del array
+
+// let comidas=["asado", "pizza", "hamburguesas"]
+// comidas.unshift("ceviche")
+// console.log(comidas)
+// agrega un elemento al principio
+
+// let comidas=["ceviche","asado", "pizza", "hamburguesas"]
+// comidas.shift()
+//console.log(comidas)
+//elimina el primer elemento
